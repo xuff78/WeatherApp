e384b5af25fb869ee079c2b256a90b3e9680cd26
xuyichao <xuyichao@infohold.com.cn>
2015-08-28 14:04:30 +0800

    图片切图

--- app/src/main/java/weather/ppx/com/weatherapp/WeatherSetting.java
+++ app/src/main/java/weather/ppx/com/weatherapp/WeatherSetting.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import weather.ppx.com.weatherapp.Fragment.BaseFragment;
@@ -15,6 +16,7 @@ import weather.ppx.com.weatherapp.View.SlipButton;
 public class WeatherSetting extends BaseActivity {
 
     SlipButton btnLocation, btnPush;
+    private ImageView themeChecked1, themeChecked2, bgImg1, bgImg2;
 
 
     @Override
@@ -30,5 +32,23 @@ public class WeatherSetting extends BaseActivity {
         btnLocation=(SlipButton)findViewById(R.id.btnLocation);
         btnPush=(SlipButton)findViewById(R.id.btnPush);
 
+        bgImg1=(ImageView)findViewById(R.id.bgImg1);
+        themeChecked1=(ImageView)findViewById(R.id.themeChecked1);
+        bgImg1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                themeChecked1.setVisibility(View.VISIBLE);
+                themeChecked2.setVisibility(View.GONE);
+            }
+        });
+        bgImg2=(ImageView)findViewById(R.id.bgImg2);
+        themeChecked2=(ImageView)findViewById(R.id.themeChecked2);
+        bgImg2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                themeChecked2.setVisibility(View.VISIBLE);
+                themeChecked1.setVisibility(View.GONE);
+            }
+        });
     }
 }
