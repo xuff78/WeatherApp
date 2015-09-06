32f649f8cd8fe8ba05afa0b57a37439a42a2eed8
xuff78 <xuff78@163.com>
2015-09-07 00:53:00 +0800

    更新请求

--- app/src/main/java/weather/ppx/com/weatherapp/Bean/WeatherInfo.java
+++ app/src/main/java/weather/ppx/com/weatherapp/Bean/WeatherInfo.java
@@ -1,7 +1,108 @@
 package weather.ppx.com.weatherapp.Bean;
 
+import common.eric.com.ebaselibrary.model.EBaseModel;
+
 /**
  * Created by 可爱的蘑菇 on 2015/9/7.
  */
-public class WeatherInfo {
+public class WeatherInfo extends EBaseModel {
+    private String dt="";
+    private String time="";
+    private String safe="";
+    private String waDT="";
+    private String tL="";
+    private String waH="";
+    private String wiDT="";
+    private String wiDV="";
+    private String wiSI="";
+    private String wiSV="";
+    private String waDV="";
+
+    public String getDt() {
+        return dt;
+    }
+
+    public void setDt(String dt) {
+        this.dt = dt;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getSafe() {
+        return safe;
+    }
+
+    public void setSafe(String safe) {
+        this.safe = safe;
+    }
+
+    public String getWaDT() {
+        return waDT;
+    }
+
+    public void setWaDT(String waDT) {
+        this.waDT = waDT;
+    }
+
+    public String gettL() {
+        return tL;
+    }
+
+    public void settL(String tL) {
+        this.tL = tL;
+    }
+
+    public String getWaH() {
+        return waH;
+    }
+
+    public void setWaH(String waH) {
+        this.waH = waH;
+    }
+
+    public String getWiDT() {
+        return wiDT;
+    }
+
+    public void setWiDT(String wiDT) {
+        this.wiDT = wiDT;
+    }
+
+    public String getWiDV() {
+        return wiDV;
+    }
+
+    public void setWiDV(String wiDV) {
+        this.wiDV = wiDV;
+    }
+
+    public String getWiSI() {
+        return wiSI;
+    }
+
+    public void setWiSI(String wiSI) {
+        this.wiSI = wiSI;
+    }
+
+    public String getWiSV() {
+        return wiSV;
+    }
+
+    public void setWiSV(String wiSV) {
+        this.wiSV = wiSV;
+    }
+
+    public String getWaDV() {
+        return waDV;
+    }
+
+    public void setWaDV(String waDV) {
+        this.waDV = waDV;
+    }
 }
